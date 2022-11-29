fbd13bb51616df5730ee83907adaf4e0d5eb69d3
Winnard Arthur <winnardarthur7@gmail.com>
2022-11-29 12:36:31 +0000

    add procfile and configurartions for heroku

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ app.get("/api/:id", async (req, res) => {
 })
 
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirnamen + "/client/build"))
+    
+    app.get("*", (req, res) => {
+        res.sendFile(__dirname + "/client/build/index.html")
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("API Running successfully")
+    })
+}
+
 
 app.listen(Port, () => {
     console.log(`Server running on port ${Port}`)
